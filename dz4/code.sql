63c2d0a5001350027809a1608d05b224ad799d45
morrigan0203 <102390174+morrigan0203@users.noreply.github.com>
2023-02-17 15:08:26 +0100

    Update code.sql

--- dz4/code.sql
+++ dz4/code.sql
@@ -5,7 +5,9 @@ select u.id, u.firstname, u.lastname, count(*) from users_communities uc left jo
 select c.id, c.name, count(*) from users_communities uc left join communities c on (uc.community_id=c.id) group by uc.community_id;
 
 -- 3. Find user who send mostly messages to user with id = 1
-select fu.id as from_id, fu.firstname as from_firstnam, fu.lastname as from_lastname, tu.firstname as to_firstname, tu.lastname as to_lastname, count(*) as count from messages m left join users fu on (m.from_user_id=fu.id) left join users tu on (m.to_user_id=tu.id) where to_user_id=1 group by from_user_id
+select fu.id as from_id, fu.firstname as from_firstnam, fu.lastname as from_lastname, tu.firstname as to_firstname, tu.lastname as to_lastname, 
+count(*) as count from messages m left join users fu on (m.from_user_id=fu.id) left join users tu on (m.to_user_id=tu.id) 
+where to_user_id=1 group by from_user_id
 order by count desc limit 1;
 
 -- 4. Count of likes for users yanger 18 years
